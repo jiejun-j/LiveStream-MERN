a01126500296d8310cd591dece0fe2155ef2ff30
Jennie <jiang.jiej@northeastern.edu>
2023-11-10 15:23:10 -0800

    Login logic

--- server/src/controllers/auth/postLogin.js
+++ server/src/controllers/auth/postLogin.js
@@ -1,3 +1,32 @@
+import User from "../../models/user.js";
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+
 export const postLogin = async(req, res) => {
-    return res.send("This is login route");
+    try{
+        const { email, password } = req.body;
+
+        const user = await User.findOne({ email: email.toLowerCase(), });
+
+        if (user && (await bcrypt.compare(password, user.password))) {
+            const token = jwt.sign(
+                { userId: user._id, email },
+                process.env.TOKEN_SECRET,
+                { expiresIn: "1d" }
+            );
+
+            return res.status(200).json({
+                userDetails: {
+                    email: user.email,
+                    username: user.username,
+                    token,
+                },
+            });
+        }
+
+        return res.status(400).send("invalid email or password");
+    } catch (err) {
+        console.log(err);
+        return res.status(500).send("error occurred, please try again");
+    }
 }
