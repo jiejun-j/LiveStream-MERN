6e50cfa6592fa9afa1c36aca406337b9182550ca
Jennie <jiang.jiej@northeastern.edu>
2023-11-11 14:40:52 -0800

    Input component

--- client/src/AuthPage/Login.js
+++ client/src/AuthPage/Login.js
@@ -1,11 +1,17 @@
 import React from "react";
 import { Logo } from "./Logo";
+import { AuthInput } from "./AuthInput";
+
 
 export const Login = ({ switchAuthHandler }) => {
     return (
         <div className="login-container">
             <Logo text={"Log in to Twitch"} />
-            <form className="auth-form">form</form>
+            <form className="auth-form">
+                <AuthInput field={"email"} label={"Email"} />
+                <AuthInput field={"password"} label={"Password"} />
+                <button>Login</button>
+            </form>
             <span onClick={switchAuthHandler} className="auth-form-switch-label">
                 Don't have an account? Sign up
             </span>
