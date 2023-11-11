ced3c846fa1e878e4d818af079e8068836bad2b0
Jennie <jiang.jiej@northeastern.edu>
2023-11-11 14:20:36 -0800

    Logo

--- client/src/AuthPage/Login.js
+++ client/src/AuthPage/Login.js
@@ -1,12 +1,14 @@
 import React from "react";
+import { Logo } from "./Logo";
 
 export const Login = ({ switchAuthHandler }) => {
-    return <div className="login-container">
-        <p>Logo</p>
-        <form className="auth-form">form</form>
-        <span onClick={switchAuthHandler} className="auth-form-switch-label">
-            Don't have an account? Sign up
-        </span>
-    </div>;
-
+    return (
+        <div className="login-container">
+            <Logo text={"Log in to Twitch"} />
+            <form className="auth-form">form</form>
+            <span onClick={switchAuthHandler} className="auth-form-switch-label">
+                Don't have an account? Sign up
+            </span>
+        </div>
+    );
 };
---
+++ client/src/AuthPage/Logo.js
@@ -0,0 +1,11 @@
+import React from "react";
+import logoPlaceholder from "../resources/images/logoPlaceholder.svg";
+
+export const Logo = ({text}) => {
+    return (
+        <div className="auth-form-logo-container">
+            <img src={logoPlaceholder} />
+            <span>&nbsp;&nbsp;{text}</span>
+        </div>
+    );
+};
