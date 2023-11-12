92a29c075ec4afb72396f78725102e4c95664049
Jennie <jiang.jiej@northeastern.edu>
2023-11-12 12:44:16 -0800

    Database models

---
+++ server/src/models/Channel.js
@@ -0,0 +1,27 @@
+import mongoose from "mongoose";
+import { v4 as uuid } from "uuid";
+
+const { Schema } = mongoose;
+
+const defaultTitle = "New Channel";
+const defaultDescription = "Welcome to my channel!";
+
+const channelSchema = new Schema({
+    isActive: { type: Boolean, default: false },
+    title: { type: String, default: defaultTitle },
+    description: { type: String, default: defaultDescription },
+    avatarUrl: { type: String, default: "none" },
+    streamKey: { type: String, default: uuid() },
+    messages: { 
+        type: [{ type: Schema.Types.ObjectId, ref: "Message" }],
+        default: [],
+    },
+});
+
+export default mongoose.model("Channel", channelSchema);
+
+
+
+
+
+
--- server/src/models/user.js
+++ server/src/models/user.js
@@ -6,6 +6,8 @@ const userSchema = new Schema({
     email: { type: String, unique: true },
     username: { type: String },
     password: { type: String },
+    channel: { type: Schema.Types.ObjectId, ref: "Channel" },
+    followedChannels: { type: [{ type: Schema.Types.ObjectId, ref: "Channel" }] },
 });
 
 export default mongoose.model("User", userSchema);
