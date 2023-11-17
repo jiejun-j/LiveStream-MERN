97970d16543d0242be76f3466eb076a562eff80e
Jennie <jiang.jiej@northeastern.edu>
2023-11-16 18:03:26 -0800

    Create navigation bar

--- client/src/DashboardPage/Nav/Nav.js
+++ client/src/DashboardPage/Nav/Nav.js
@@ -1,5 +1,32 @@
 import React from "react";
+import logo from "../../resources/images/logoPlaceholder.svg";
+
+const NavLogo = () => {
+    return <div className="nav-logo-container">
+        <img className="nav-logo" width="100%" height="100%" src={ logo } />
+    </div>;
+}
+
+const NavButton = ({ text, onClickHandler }) => {
+    return (
+        <span className="nav-button" onClick={ onClickHandler }>
+            { text }
+        </span>
+    );
+};
 
 export const Nav = () => {
-    return <div className="nav-container">Nav</div>;
-};
+    return (
+        <div className="nav-container">
+            <NavLogo />
+            <div className="nav-buttons-container">
+                <NavButton text="Browse" onClickHandler={ () => {} } />
+                <NavButton text="Login" onClickHandler={ () => {} } />
+                <div>
+                    <NavButton text="My Account" onClickHandler={ () => {} } />
+                    <NavButton text="Logout" onClickHandler={ () => {} } />
+                </div>
+            </div>
+        </div>
+    );
+}
