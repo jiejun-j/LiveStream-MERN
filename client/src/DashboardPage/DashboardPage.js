180029b192ea9577d2d9c9ec3aa61d18406925c4
Jennie <jiang.jiej@northeastern.edu>
2023-11-16 16:07:21 -0800

    Update Router

--- client/src/DashboardPage/DashboardPage.js
+++ client/src/DashboardPage/DashboardPage.js
@@ -1,5 +1,7 @@
 import React from "react";
 
+import "./dashboardPage.css";
+
 export const DashboardPage = () => {
     return <div>This is dashboard page</div>;
 };
--- client/src/index.js
+++ client/src/index.js
@@ -1,23 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import { AuthPage } from './AuthPage';
 import { DashboardPage } from './DashboardPage';
 import { App } from './App';
 
 import './index.css';
 
-const router = createBrowserRouter([
-  { path: '/auth', element: <AuthPage /> },
-  { path: '/', element: <DashboardPage /> },
-]);
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  <BrowserRouter>
     <App>
-      <RouterProvider router={router} />
+      <Routes>
+        <Route path="/auth" element={<AuthPage />} />
+        <Route path="*" element={<DashboardPage />} />
+      </Routes>
     </App>
-  </React.StrictMode>
+  </BrowserRouter>
 );
-
