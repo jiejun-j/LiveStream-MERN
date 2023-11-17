b79aacc19e29edc5507a6fc357ec236c0048df14
Jennie <jiang.jiej@northeastern.edu>
2023-11-16 18:19:16 -0800

    Sidebar - List of Followed Channels

--- client/src/DashboardPage/Sidebar/Sidebar.js
+++ client/src/DashboardPage/Sidebar/Sidebar.js
@@ -1,5 +1,40 @@
 import React from "react";
 
+const followedChannels = [
+    {
+        id: 1,
+        username: "user1",
+        isOnline: true
+    },
+    {
+        id: 2,
+        username: "user2",
+        isOnline: false
+    },
+    {
+        id: 3,
+        username: "user3",
+        isOnline: true
+    },
+];
+
 export const Sidebar = () => {
-    return <div className="sidebar-container">Sidebar</div>;
+    return (
+        <div className="sidebar-container">
+            <span className="sidebar-title">For you</span>
+            <span className="sidebar-subtitle">FOLLOWED CHANNRLS</span>
+            {followedChannels.map((channel) => {
+                return (
+                    <div key={channel.id} className="sidebar-list-item">
+                        <span className="sidebar-list-username">{channel.username}</span>
+                        <span 
+                            className="sidebar-list-status"
+                            style={{ 
+                                color: channel.isOnline ? "green" : "red" }}
+                        >{channel.isOnline ? "Online" : "Offline"}</span>
+                    </div>
+                );
+            })}
+        </div >
+    );
 };
