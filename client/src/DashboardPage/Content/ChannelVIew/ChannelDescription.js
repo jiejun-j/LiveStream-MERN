16feca5698ee50f797e130789c806477171ad8b7
Jennie <jiang.jiej@northeastern.edu>
2023-11-20 10:58:12 -0800

    Display follow button
    Only login user can see the button

--- client/src/DashboardPage/Content/ChannelVIew/ChannelDescription.js
+++ client/src/DashboardPage/Content/ChannelVIew/ChannelDescription.js
@@ -1,10 +1,33 @@
 import React from "react";
+import { useUserDetails } from "../../../shared/hooks";
 
-export const ChannelDescription = ({username, title, description}) => {
+const FollowButton = ({ channelId }) => {
+    return (
+        <button className="channel-follow-button">
+            Follow
+        </button>
+    );
+}
+
+export const ChannelDescription = ({
+    username, 
+    title, 
+    description, 
+    channelId
+}) => {
+    const { isLogged } = useUserDetails();
+
     return(
         <div className="channel-description-container">
             <span className="channel-description-title">
                 {username}
+                <span>
+                    {isLogged &&
+                        <FollowButton
+                        className="channel-follow-button"
+                        channelId={channelId}
+                    />}
+                </span>
             </span>
             <span className="channel-description-subtitle">{title}</span>
             <div className="channel-description-box">
