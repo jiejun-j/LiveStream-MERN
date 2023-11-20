ece2508079cccbe258fd16a046156dfe852b7870
Jennie <jiang.jiej@northeastern.edu>
2023-11-19 21:19:19 -0800

    change default avataUrl

--- server/src/models/Channel.js
+++ server/src/models/Channel.js
@@ -10,7 +10,7 @@ const channelSchema = new Schema({
     isActive: { type: Boolean, default: false },
     title: { type: String, default: defaultTitle },
     description: { type: String, default: defaultDescription },
-    avatarUrl: { type: String, default: "none" },
+    avatarUrl: { type: String, default: "https://assets.nintendo.com/image/upload/ar_16:9,b_auto:border,c_lpad/b_white/f_auto/q_auto/dpr_1.0/c_scale,w_1200/ncom/software/switch/70010000027619/9989957eae3a6b545194c42fec2071675c34aadacd65e6b33fdfe7b3b6a86c3a" },
     streamKey: { type: String, default: uuid() },
     messages: { 
         type: [{ type: Schema.Types.ObjectId, ref: "Message" }],
