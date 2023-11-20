c06957f7892e154053dd1935052f820ca0a68d86
Jennie <jiang.jiej@northeastern.edu>
2023-11-19 21:07:27 -0800

    fix login parsing fails

--- client/src/shared/hooks/useUserDetails.js
+++ client/src/shared/hooks/useUserDetails.js
@@ -4,10 +4,17 @@ import { logout as logoutHandler } from "../utils";
 const getUserDetails = () => {
     const userDetails = localStorage.getItem('user');
     if (userDetails) {
-        return JSON.parse(userDetails);
+        try {
+            return JSON.parse(userDetails);
+        } catch (error) {
+            console.error('Error parsing user details:', error);
+            // When parsing fails, you can choose to clear the invalid local storage item
+            localStorage.removeItem('user');
         }
+    }
     return null;
 }
+
 
 export const useUserDetails = () => {
     const [userDetails, setUserDetails] = useState(getUserDetails());
