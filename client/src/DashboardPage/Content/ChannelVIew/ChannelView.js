39cd4d90562daf2bd9337e5fa2fe5f1f76aef8b7
Jennie <jiang.jiej@northeastern.edu>
2023-12-23 13:09:55 -0800

    Hardcode Stream URL

--- client/src/DashboardPage/Content/ChannelVIew/ChannelView.js
+++ client/src/DashboardPage/Content/ChannelVIew/ChannelView.js
@@ -1,9 +1,20 @@
 import React, { useEffect } from "react";
 import { useParams } from "react-router-dom";
+import { ReactFlvPlayer } from "react-flv-player";
 import { Chat } from "./Chat";
 import { ChannelDescription } from "./ChannelDescription";
 import { useChannelDetails } from "../../../shared/hooks";
 import { LoadingSpinner } from "../../../shared/components";
+
+export const Stream = () => {
+    return <div className="channel-video-container">
+        <ReactFlvPlayer
+            url="http://localhost:8000/live/9ba028af-dcf3-451e-b73c-46198000d928.flv"
+            height="100%"
+            width="100%"
+        />
+    </div>;
+};
 
 export const ChannelView = () => {
     const { isFetching, getChannelDetails, channelDetails } = useChannelDetails();
@@ -23,9 +34,10 @@ export const ChannelView = () => {
     return(
         <div className="channel-container">
             <div className="channel-video-description-section">
-                <div className="channel-offline-placeholder">
+                {/* <div className="channel-offline-placeholder">
                     <span>Channel is offline</span>
-                </div>
+                </div> */}
+                <Stream />
                 <ChannelDescription
                     channelId={channelDetails.id}
                     title={channelDetails.title}
