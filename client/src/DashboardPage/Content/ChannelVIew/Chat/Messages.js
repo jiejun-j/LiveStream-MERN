cfcd049203529b8ed120b056fc7e8c9de114083d
Jennie <jiang.jiej@northeastern.edu>
2024-01-04 12:01:09 -0800

    Message container

--- client/src/DashboardPage/Content/ChannelVIew/Chat/Messages.js
+++ client/src/DashboardPage/Content/ChannelVIew/Chat/Messages.js
@@ -1,5 +1,40 @@
 import React from "react";
 
+const messages = [
+    {
+        author: "John",
+        content: "Hello!",
+        id: 1,
+    },
+    {
+        author: "John",
+        content: "Hello!",
+        id: 2,
+    },
+    {
+        author: "John",
+        content: "Hello!",
+        id: 3,
+    },
+];
+
+const Message = ({ author, content }) => {
+    return(
+    <span className="chat-messages-message">
+        <span style = {{fontWeight:'bold'}}>{author}: </span>
+        {content}
+    </span>
+    )
+}
+
 export const Messages = () => {
-    return <div>Input</div>;
+    return <div className="chat-messages-container">
+        {messages.map((message) => (
+            <Message
+                key={message.id}
+                author={message.author}
+                content={message.content}
+            />
+        ))}
+    </div>;
 };
