10429e5b8acc6ba1e0424fbc167225e1eb975449
Jennie <jiang.jiej@northeastern.edu>
2024-01-04 11:41:11 -0800

    Create message component

--- client/src/DashboardPage/Content/ChannelVIew/Chat/Chat.js
+++ client/src/DashboardPage/Content/ChannelVIew/Chat/Chat.js
@@ -1,9 +1,15 @@
 import React from "react";
+import { Messages } from "./Messages";
+import { NewMessageInput } from "./NewMessageInput";
 
-export const Chat = () => {
+export const Chat = ({ channelId }) => {
     return (
         <div className="chat-section">
-
+            <div className="chat-title-container">
+                <span className="chat-title-text">Stream Chat</span>
+            </div>
+            <Messages messages={[]} />
+            <NewMessageInput sendMessage={() => {}} />
         </div>
     );
 };    
---
+++ client/src/DashboardPage/Content/ChannelVIew/Chat/Messages.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export const Messages = () => {
+    return <div>Input</div>;
+};
