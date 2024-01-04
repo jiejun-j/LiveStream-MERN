bbceff15810530785115b35e15260db6f230b82a
Jennie <jiang.jiej@northeastern.edu>
2024-01-04 12:11:43 -0800

    New message input

--- client/src/DashboardPage/Content/ChannelVIew/Chat/NewMessageInput.js
+++ client/src/DashboardPage/Content/ChannelVIew/Chat/NewMessageInput.js
@@ -1,5 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 
-export const NewMessageInput = () => {
-    return <div>Input</div>;
+export const NewMessageInput = ({ sendMessage}) => {
+    const [messageContent, setMessageContent] = useState("");
+
+    const handleValueChange = (e) =>{
+        setMessageContent(e.target.value);
+    };
+
+    const handleSendMessage = () => {
+        // send message to the server
+
+        // after message is sent, clear the input field
+        setMessageContent("");
+    };
+
+    const handleKeyPress = (e) => {
+        if (e.key === "Enter") {
+            handleSendMessage();
+        }
+    };
+
+
+    return <div className="chat-message-input-container">
+        <input
+            className="chat-message-input"
+            placeholder="Type message here..."
+            value={messageContent}
+            onChange={handleValueChange}
+            onKeyDown={handleKeyPress}
+        />
+    </div>;
 };
